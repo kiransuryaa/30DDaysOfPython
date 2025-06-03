56e45f87c27e0a9e43c4a1c997aa6f284d57a53e
Kiran Suryawanshi <141052509+kiransuryaa@users.noreply.github.com>
2025-06-03 15:59:48 +0530

    Add files via upload

---
+++ 30DaysPythonLearning/mymodule.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Creating different functions in my module (i.e my_module)
+# This module is used to calculate area of diffrent shapes.
+# It also includes some other simple functions like, greeting, created dictionary of student.
+
+def greet_user(name):
+    return f"Hello, {name}! Welcome!"
+
+def calculate_area_triangle(base, height):
+    triangle_area = round((length (1/2) * base * height) , 2 )
+    return triangle_area
+
+def calculate_area_square(side):
+    square_area = side * side
+    return square_area
+
+def calculate_area_rectangle(length, width):
+    rect_area = length * width
+    return rect_area
+
+# creating student dictionary
+student = {"name" : "Asha", "Age" : 16, "class": 10, "City" : "pune"}
+
